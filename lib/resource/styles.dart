c955903bac5b8be90de474d459131aca6ebb0712
SinirliJapon <mkemalerylmz@gmail.com>
2024-01-21 12:46:44 +0100

    new resources added

--- lib/resource/styles.dart
+++ lib/resource/styles.dart
@@ -6,4 +6,12 @@ abstract class Styles {
 
   static const cafeTitleTextStyle = TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold);
   static const cafeSubtitleTextStyle = TextStyle(color: Colors.black, fontSize: 16);
+
+  static const orderTitleTextStyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+  static const orderSubtitleTextStyle = TextStyle(fontSize: 16, fontWeight: FontWeight.w400);
+  static const orderTrailingTextStyle = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+
+  static const drawerTitleTextStyle = TextStyle(color: Colors.brown, fontSize: 18, fontWeight: FontWeight.bold);
+
+  static const drawerHeaderTextStyle = TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold);
 }
--- lib/resource/theme_color.dart
+++ lib/resource/theme_color.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 abstract class ThemeColor {
   static const primaryColor = Colors.brown;
   static const foregroundColor = Colors.white;
+
+  static Color? tileColor = Colors.grey[200];
 }
